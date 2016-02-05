f15387bac17cd919747fb6ff22e5af81e8154c39
ryanjamesmcgill <littlelo.music@gmail.com>
2016-02-05 05:58:11 +0000

    edit scan step coloring for demo data

--- worklist-generator.js
+++ worklist-generator.js
@@ -76568,8 +76568,8 @@ function setMasterSteps(stepArray) {
 
 		MasterSteps[i].workliststatus = false;
 		var isScan;
-		//if(MasterSteps[i].ppid.indexOf("SCAN")>-1){ //fake data
-		if (MasterSteps[i].ppid.indexOf("@PART") > -1) {
+		if(MasterSteps[i].ppid.indexOf("SCAN")>-1){ //fake data
+		//if (MasterSteps[i].ppid.indexOf("@PART") > -1) {
 			//real data
 			isScan = true;
 		} else {
